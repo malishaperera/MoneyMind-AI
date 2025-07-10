f9cc1630f673d22254a054cd265435e8f1ddab9c
Malisha Perera <malishashavinda577@gmail.com>
2025-07-11 00:04:43 +0530

    Implement styled transaction table with sorting, selection, category labels, and recurring status✅

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -1,5 +1,8 @@
 import { getAccountWithTransactions } from "@/actions/account";
 import { notFound } from "next/navigation";
+import {Suspense} from "react";
+import TransactionTable from "@/app/(main)/account/_components/transaction-table";
+import {BarLoader} from "react-spinners";
 
 const AccountsPage = async ({ params }) => {
     const awaitedParams = await params;
@@ -14,19 +17,37 @@ const AccountsPage = async ({ params }) => {
     const { transactions, ...account } = accountData;
 
     return (
-       <div>
+       <div className="space-y-8 px-5">
+           <div className="flex gap-4 items-end justify-between">
            <div>
-               <h1>{account.name}</h1>
+               <h1 className="text-5xl sm:text-6xl font-bold  capitalize">
+                   {account.name}
+               </h1>
                <p>{account.type.charAt(0).toUpperCase() + account.type.slice(1).toLowerCase()} Account</p>
-
            </div>
-
-           <div>
-               <div>
+           <div className="text-right pb-2">
+               <div className="text-xl sm:text-2xl font-bold">
                    ${parseFloat(account.balance).toFixed(2)}
                </div>
-               <p>{account._count.transactions}Transaction</p>
+               <p className="text-xl sm:text-2xl font-bold">
+                   {account._count.transactions}Transaction</p>
            </div>
+
+           </div>
+           {/*Chart*/}
+           {/*Transaction*/}
+           <Suspense
+           fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea"/>}>
+               <TransactionTable transaction={transactions}/>
+           </Suspense>
+
+
+
+
+
+
+
+
        </div>
     );
 };
